e2d9ab87cf3da5795eb48ad03ae1395f9aa6a013
gmalmada <gonzaaalmada6401@gmail.com>
2026-03-26 17:50:22 -0300

    .

--- src/main/java/com/mycompany/primeraAPI/model/Persona.java
+++ src/main/java/com/mycompany/primeraAPI/model/Persona.java
@@ -1,6 +1,16 @@
 package com.mycompany.primeraAPI.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Persona {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nombre;
     private int edad;
